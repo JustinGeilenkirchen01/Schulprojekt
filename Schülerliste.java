8ca126b1bca049b4595a6863ab624caf8f7b8ca1
JustinGeilenkirchen01 <70322828+JustinGeilenkirchen01@users.noreply.github.com>
2020-10-02 15:45:38 +0200

    Add files via upload

---
+++ Schülerliste.java
@@ -0,0 +1,105 @@
+
+public class Schülerliste {
+public static void main(String[] args) {
+	   
+	   
+	   System.out.println("Schülertabelle");
+	   
+System.out.println(""); // Leerzeile
+	   
+//Schülernamen
+	   
+	   String Name1 = "Peter Pan";
+	   String Name2 = "Jonas Peters";
+	   String Name3 = "Heike Lohmann";
+	   String Name4 = "Johanna Pc";
+	   String Name5 = "Jan LOL";
+	   String Name6 = "Kan Öllers";
+	   
+//Alter
+	   
+	   int Alter1 = 27;
+	   int Alter2 = 18;
+	   int Alter3 = 19;
+	   int Alter4 = 16;
+	   int Alter5 = 20;
+	   int Alter6 = 15;
+	   
+	   System.out.println( Name1 + " ist ein Schüler.");
+	   System.out.println("Er ist " + Alter1 + " Jahre alt.");
+	   
+       if   (Alter1<18) {
+       System.out.println("Er ist minderjährig.");
+       }else {
+	   System.out.println("Er ist volljährig.");
+       }
+	   
+	   
+System.out.println(""); // Leerzeile
+	   
+	   System.out.println( Name2 + " ist ein Schüler.");
+	   System.out.println("Er ist " + Alter2 + " Jahre alt. ");
+	   
+	   if   (Alter2<18) {
+	       System.out.println("Er ist minderjährig.");
+	       }else {
+		   System.out.println("Er ist volljährig.");
+	       }
+		   
+	   
+System.out.println(""); // Leerzeile
+
+	   System.out.println( Name3 + " ist eine Schülerin." );
+	   System.out.println("Sie ist " + Alter3 + " Jahre alt.");
+	  
+	   if   (Alter3<18) {
+	       System.out.println("Sie ist minderjährig.");
+	       }else {
+		   System.out.println("Sie ist volljährig.");
+	       }
+		   
+	   
+System.out.println(""); // Leerzeile
+ 
+	   System.out.println( Name4 + " Sie ist eine Schülerin.");
+	   System.out.println( "Sie ist " + Alter4 + " Jahre alt.");
+	   
+	   if   (Alter4<18) {
+	       System.out.println("Sie ist minderjährig.");
+	       }else {
+		   System.out.println("Sie ist volljährig.");
+	       }
+		   	   
+	   
+System.out.println(""); // Leerzeile
+			      	   
+	   System.out.println( Name5 + " Er ist ein Schüler.");
+	   System.out.println( "Er ist " + Alter5 + " Jahre alt.");
+	   
+	   if   (Alter5<18) {
+	       System.out.println("Er ist minderjährig.");
+	       }else {
+		   System.out.println("Er ist volljährig.");
+	       }
+		   
+	   
+System.out.println(""); // Leerzeile	   
+	   	   
+       System.out.println( Name6 + " Er ist ein Schüler");	
+       System.out.println( "Er ist " + Alter6 + " Jahre alt.");
+      
+	   
+       if   (Alter6<18) {
+       System.out.println("Er ist minderjährig.");
+       }else {
+	   System.out.println("Er ist volljährig.");
+	   
+       }
+       
+   }
+
+}
+
+
+
+
